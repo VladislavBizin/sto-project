648cce5192546e9cf014e0496b58ecc4f019e64f
Oleg-Melnikow <melnikow.olezhka@mail.ru>
2019-09-14 19:01:35 +0300

    add DAL

---
+++ front/src/DAL/orders.api.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+
+const instance = axios.create({
+    baseURL: "http://localhost:3004"
+
+});
+
+
+export const ordersAPI = {
+    getOrders() {
+        return instance.get("orders");
+    }
+};
