6ae7bc1fb8e9e05595929cf0337233c2c5ddef65
Siarhei <spartateambuilder@mail.ru>
2019-09-14 19:57:15 +0300

    fix bugs

--- front/src/index.js
+++ front/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Provider from 'react-redux';
+import {Provider} from 'react-redux';
 import store from './redux/store';
-import BrowserRouter from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 
 
 ReactDOM.render(
